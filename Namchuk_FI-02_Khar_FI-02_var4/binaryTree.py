006d7ba692fcf115e8dd3c47c8a73093f6b71b89
ipt-cryptolabs <ipt.cryptolabs@gmail.com>
2023-01-15 16:37:28 +0200

    [V-4] Namchuk (FI-02), Khar (FI-02) (#8)

---
+++ Namchuk_FI-02_Khar_FI-02_var4/binaryTree.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, path, data):
+        if self.root == None:
+            self.root = Node(data)
+            return
+        else:
+            if [*data][0] == [*path.data][0]:
+                path.data[[*path.data][0]].append(*data[[*data][0]])
+                return
+            if path:
+                if [*path.data][0] > [*data][0]:
+                    if path.left != None:
+                        path = path.left
+                        self.insert(path, data)
+                    else:
+                        if path.left != None:
+                            data[[*data][0]] = data[[*data][0]][0]
+                        path.left = Node(data)
+                else:
+                    if path.right != None:
+                        path = path.right
+                        self.insert(path, data)
+                    else:
+                        if path.right != None:
+                            data[[*data][0]] = data[[*data][0]][0]
+                        path.right = Node(data)
+            else:
+                path = Node(data)
+    def search_equal(self, value):
+        acc = []
+        self._search_equal(value, acc, self.root)
+        return acc
+    def _search_equal(self, value, acc, path):
+        if path is None:
+            return
+        if [*path.data][0] == value:
+            for i in path.data[[*path.data][0]]:
+                acc.append(i)
+        if [*path.data][0] > value:
+            self._search_equal(value, acc, path.left)
+            self._search_equal(value, acc, path.right)
+        else:
+            self._search_equal(value, acc, path.right)
+    def search_more(self, value):
+        acc = []
+        self._search_more(value, acc, self.root)
+        return acc
+    def _search_more(self, value, acc, path):
+        if path is None:
+            return
+        if [*path.data][0] > value:
+            for i in path.data[[*path.data][0]]:
+                acc.append(i)
+        if [*path.data][0] > value:
+            self._search_more(value, acc, path.left)
+            self._search_more(value, acc, path.right)
+        else:
+            self._search_more(value, acc, path.right)
